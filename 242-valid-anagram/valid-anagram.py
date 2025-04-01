02c87ab90d47348562d66cace9fec48ad7ef64f4
Manjay Verma <161963231+manjaycoder@users.noreply.github.com>
2025-04-01 19:09:41 +0530

    Time: 11 ms (72.49%) | Memory: 17.8 MB (86.57%) - LeetSync

--- 242-valid-anagram/valid-anagram.py
+++ 242-valid-anagram/valid-anagram.py
@@ -1,12 +1,12 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:    
-        count=defaultdict(int)
-        for x in s:
-            count[x]+=1
+        nums=defaultdict(int)
         for x in t:
-            count[x]-=1
-        for val in count.values():
-            if val != 0:
+            nums[x] +=1
+        for x in s:
+            nums[x] -=1
+        for val in nums.values():
+            if val !=0:
                 return False
 
         return True
