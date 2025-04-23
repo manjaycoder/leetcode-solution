061bb26a0ad3aab1fcad26efde2a72cb8be4beec
Manjay Verma <161963231+manjaycoder@users.noreply.github.com>
2025-04-23 18:17:33 +0530

    Time: 9 ms (80.43%) | Memory: 57.3 MB (91.91%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.js
+++ 347-top-k-frequent-elements/top-k-frequent-elements.js
@@ -4,14 +4,13 @@
  * @return {number[]}
  */
 var topKFrequent = function(nums, k) {
-     const count = {};
-        for (const num of nums) {
-            count[num] = (count[num] || 0) + 1;
-        }
+    const count={}
+    for(let num of nums){
+        count[num]=(count[num] || 0)+1;
 
-        const arr = Object.entries(count).map(([num, freq]) => [freq, parseInt(num)]);
-        arr.sort((a, b) => b[0] - a[0]);
-
-        return arr.slice(0, k).map(pair => pair[1]);
+    }
+    let arr=Object.entries(count).map(([freq,num])=>[num ,parseInt(freq)])
+    arr.sort((a,b)=>b[0]-a[0])
+    return arr.slice(0,k).map(pair=>pair[1])
     
 };
