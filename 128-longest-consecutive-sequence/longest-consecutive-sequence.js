598dfc49e9acc1d08144e5dc39f4e7cb5b3c0774
Manjay Verma <161963231+manjaycoder@users.noreply.github.com>
2025-04-23 18:50:55 +0530

    Time: 32 ms (95.11%) | Memory: 81.5 MB (26.65%) - LeetSync

---
+++ 128-longest-consecutive-sequence/longest-consecutive-sequence.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @return {number}
+ */
+var longestConsecutive = function(nums) {
+    const numSet = new Set(nums);
+        let longest = 0;
+
+        for (let num of numSet) {
+            if (!numSet.has(num - 1)) {
+                let length = 1;
+                while (numSet.has(num + length)) {
+                    length++;
+                }
+                longest = Math.max(longest, length);
+            }
+        }
+        return longest;
+};
