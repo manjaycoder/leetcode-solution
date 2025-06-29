56d377a19a9bc827f0ae81dec804febbb0b0131c
Manjay-webdev <161963231+manjaycoder@users.noreply.github.com>
2025-06-29 16:59:45 +0530

    Time: 2 ms (57.40%) | Memory: 56.6 MB (28.70%) - LeetSync

--- 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.js
+++ 1951-find-the-winner-of-the-circular-game/find-the-winner-of-the-circular-game.js
@@ -3,16 +3,21 @@
  * @param {number} k
  * @return {number}
  */
- var solve =function (k,arr,i){
-  if(arr.length ==1) return arr[0];
-  i=(i+k)%arr.length
-  arr.splice(i,1);
-  return solve(k,arr,i) 
- }
+var solve=function(i,arr,k){
+if(arr.length===1) return arr[0]
+i=(i+k)%arr.length
+arr.splice(i,1)
+return solve(i,arr,k)
+
+}
 var findTheWinner = function(n, k) {
-    let winner = 0;
-  for (let i = 2; i <= n; i++) {
-    winner = (winner + k) % i;
-  }
-  return winner + 1; 
-};
+    let arr=new Array(n);
+    for(let i=0;i<n;i++){
+        arr[i]=i+1
+    }
+k--;
+return solve(0,arr,k)
+
+
+
+}
