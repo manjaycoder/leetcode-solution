7269bc7d59cc6b258d50d8b324d952b86364d346
Manjay-webdev <161963231+manjaycoder@users.noreply.github.com>
2025-07-05 18:20:21 +0530

    Time: 2 ms (73.02%) | Memory: 58 MB (30.97%) - LeetSync

--- 169-majority-element/majority-element.js
+++ 169-majority-element/majority-element.js
@@ -6,11 +6,10 @@ var majorityElement = function(nums) {
     let res=0;
     let majority=0;
     for(let n of nums){
-        if(majority ===0){
-            res=n;
-            
+        if(majority ==0){
+            res=n
         }
-        majority += (n===res)?1:-1;
+        majority += (res===n)?1:-1;
     }
     return res
 };
