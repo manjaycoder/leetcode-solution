00b34ce7a8a5ac2e6b1e07032fdc8bfc26937c9a
Manjay-webdev <161963231+manjaycoder@users.noreply.github.com>
2025-07-17 12:36:24 +0530

    Time: 0 ms (100.00%) | Memory: 17.7 MB (62.89%) - LeetSync

--- 3371-harshad-number/harshad-number.py
+++ 3371-harshad-number/harshad-number.py
@@ -1,4 +1,4 @@
 class Solution:
     def sumOfTheDigitsOfHarshadNumber(self, x: int) -> int:
-        sumX=sum(int(d) for d in list(str(x)))
-        return sumX if x%sumX==0 else -1
+        sumX=sum(int(d) for d in str(x))
+        return sumX if x%sumX ==0 else -1
