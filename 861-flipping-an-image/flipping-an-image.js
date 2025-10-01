82d634121ff8bd2aa16a703d1f39c313324d48c3
Manjay-webdev <manjay.verma.coder@gmail.com>
2025-10-01 19:14:21 +0530

    Time: 1 ms (52.86%) | Memory: 58.4 MB (8.15%) - LeetSync

--- 861-flipping-an-image/flipping-an-image.js
+++ 861-flipping-an-image/flipping-an-image.js
@@ -3,5 +3,21 @@
  * @return {number[][]}
  */
 var flipAndInvertImage = function(image) {
-    return image.map(e=>e.reverse().map(i=>i==0?1:0))
+    // First: flip each row (reverse the row)
+    for (let i = 0; i < image.length; i++) {
+        image[i].reverse();
+    }
+
+    // Second: invert the image (0 -> 1, 1 -> 0)
+    for (let i = 0; i < image.length; i++) {
+        for (let j = 0; j < image[i].length; j++) {
+            if (image[i][j] === 0) {
+                image[i][j] = 1;
+            } else {
+                image[i][j] = 0;
+            }
+        }
+    }
+
+    return image;
 };
