2314c10e058d56e85ee3a4ace09b834d8008eff0
Manjay-webdev <manjay.verma.coder@gmail.com>
2025-10-01 15:54:40 +0530

    Time: 2 ms (47.44%) | Memory: 56.2 MB (89.98%) - LeetSync

--- 898-transpose-matrix/transpose-matrix.js
+++ 898-transpose-matrix/transpose-matrix.js
@@ -3,11 +3,12 @@
  * @return {number[][]}
  */
 var transpose = function(matrix) {
-    let arr=Array.from({length:matrix[0].length},()=>Array(matrix.length))
-    for(let i=0;i<arr.length;i++){
-        for(let j=0;j<arr[i].length;j++){
+    let arr=Array.from({length:matrix[0].length},()=>Array(matrix.length));
+    for(let i=0;i<=arr.length-1;i++){
+        for(let j=0;j<=arr[0].length-1;j++){
             arr[i][j]=matrix[j][i]
         }
     }
     return arr
+
 };
