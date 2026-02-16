f01fa7d903bb9ef2b33d32bd193d34d6cf9d344e
Manjay-webdev <manjay.verma.coder@gmail.com>
2026-02-16 13:54:10 +0530

    Time: 3 ms (91.08%), Space: 46.5 MB (41.66%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,23 +1,12 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-       //if the length are different, they can't be anagrams
-       if(s.length() != t.length()){
-        return false;
-       }
-       //create an array to count character frequencies
-       int[] charCounts=new int[26];//assuming only lowerCase english letter
-       //increment count for each character in 's' and decrement for each in 't'
-       for(int i=0;i<s.length();i++){
-        charCounts[s.charAt(i)-'a']++;
-        charCounts[t.charAt(i)-'a']--;
-       }
-    //check if all counts are zero
-    for(int count:charCounts){
-        if(count!=0){
-            return false;
-        }
-    }
-    return true;
+     char chars[]=s.toCharArray();
+     char chars1[]=t.toCharArray();
+     Arrays.sort(chars);
+     Arrays.sort(chars1);
+     String s1=new String(chars);
+     String s2=new String(chars1);
+     return (s1.equals(s2))?true:false;
     }
 
 }
