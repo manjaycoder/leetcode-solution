f9520d28ea80f8a0583d509b508860a473a753a5
Manjay-webdev <manjay.verma.coder@gmail.com>
2026-05-29 22:22:54 +0530

    Time: 20 ms (45.66%), Space: 25.8 MB (56.4%) - LeetHub

---
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -0,0 +1,10 @@
+class Solution(object):
+    def containsDuplicate(self, nums):
+        hset=set()
+        for idx in nums:
+            if idx in hset:
+                return True
+            else:
+                hset.add(idx)
+        return False
+        
