b51124153272821da3d12c98c0686f669795cdaf
Manjay-webdev <manjay.verma.coder@gmail.com>
2026-06-01 22:54:30 +0530

    Time: 5 ms (62.51%), Space: 44.4 MB (74.3%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.java
+++ 0242-valid-anagram/0242-valid-anagram.java
@@ -1,11 +1,21 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-          if (s.length() != t.length()) return false;
+        if(s.length() != t.length()) return false;
+        //create an array to count character frequencies
+        int[] charCounts = new int[26];
+        //Increment count for erach chracater in 's' and decrement for each in 't'
+        for(int i=0;i<s.length();i++){
+            charCounts[s.charAt(i)-'a']++;
+            charCounts[t.charAt(i)-'a']--;
+
+        }
+            //check if all counts are zero
+        for(int count :charCounts){
+            if(count != 0){
+                return false;
+            }
     
-    char[] sArr = s.toCharArray();
-    char[] tArr = t.toCharArray();
-    Arrays.sort(sArr);
-    Arrays.sort(tArr);
-    return Arrays.equals(sArr, tArr);
+        }
+        return true;
+    }    
     }
-}
