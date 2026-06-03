a1f3ecbfbacb23ad7a270c4bb6de1f9ce82941fb
Manjay-webdev <manjay.verma.coder@gmail.com>
2026-06-03 21:05:29 +0530

    Time: 4 ms (48.11%), Space: 13.1 MB (67.42%) - LeetHub

---
+++ 0001-two-sum/0001-two-sum.py
@@ -0,0 +1,15 @@
+class Solution(object):
+    def twoSum(self, nums, target):
+        """
+        :type nums: List[int]
+        :type target: int
+        :rtype: List[int]
+        """
+        seen={}
+        for i, num in enumerate(nums):
+            complement=target-num
+            if complement in seen:
+                return [seen[complement],i]
+            seen[num]=i
+            
+        
